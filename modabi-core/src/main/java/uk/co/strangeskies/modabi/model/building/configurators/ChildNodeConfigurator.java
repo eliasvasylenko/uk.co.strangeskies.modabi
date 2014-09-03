c7139e41e2fb18bfc77b363b7ef2014bf615383d
eliasvasylenko <eliasvasylenko@gmail.com>
2014-09-03 11:58:04 +0100

    documentation for child node congfigurator naming

--- modabi-core/src/main/java/uk/co/strangeskies/modabi/model/building/configurators/ChildNodeConfigurator.java
+++ modabi-core/src/main/java/uk/co/strangeskies/modabi/model/building/configurators/ChildNodeConfigurator.java
@@ -4,5 +4,12 @@ import uk.co.strangeskies.modabi.model.nodes.SchemaNode;
 
 public interface ChildNodeConfigurator<S extends ChildNodeConfigurator<S, N>, N extends SchemaNode<?, ?>>
 		extends SchemaNodeConfigurator<S, N> {
+	/**
+	 * Here we can just provide a string name instead of a fully qualified name,
+	 * and the namespace of the parent node will be used.
+	 *
+	 * @param name
+	 * @return
+	 */
 	public S name(String name);
 }
