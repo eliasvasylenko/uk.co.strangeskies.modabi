d52c0b66e54c75170cbbf2ae12b852f48685e8ce
eliasvasylenko <eliasvasylenko@gmail.com>
2014-10-16 21:24:11 +0100

    Merge

--- modabi-core/src/main/java/uk/co/strangeskies/modabi/schema/node/building/configuration/DataNodeConfigurator.java
+++ modabi-core/src/main/java/uk/co/strangeskies/modabi/schema/node/building/configuration/DataNodeConfigurator.java
@@ -2,17 +2,14 @@ package uk.co.strangeskies.modabi.schema.node.building.configuration;
 
 import uk.co.strangeskies.modabi.io.DataSource;
 import uk.co.strangeskies.modabi.schema.node.DataNode;
-import uk.co.strangeskies.modabi.schema.node.DataNodeChildNode;
 import uk.co.strangeskies.modabi.schema.node.DataNode.Format;
 import uk.co.strangeskies.modabi.schema.node.type.DataBindingType;
 import uk.co.strangeskies.modabi.schema.processing.ValueResolution;
 
-public interface DataNodeConfigurator<T>
-		extends
-		BindingChildNodeConfigurator<DataNodeConfigurator<T>, DataNode<T>, T, DataNodeChildNode<?, ?>, DataNode<?>>,
-		SchemaNodeConfigurator<DataNodeConfigurator<T>, DataNode<T>, DataNodeChildNode<?, ?>, DataNode<?>> {
-	public <U extends T> DataNodeConfigurator<U> type(
-			DataBindingType<? super U> type);
+public interface DataNodeConfigurator<T> extends
+		BindingChildNodeConfigurator<DataNodeConfigurator<T>, DataNode<T>, T>,
+		SchemaNodeConfigurator<DataNodeConfigurator<T>, DataNode<T>> {
+	public <U extends T> DataNodeConfigurator<U> type(DataBindingType<U> type);
 
 	@Override
 	public <U extends T> DataNodeConfigurator<U> dataClass(Class<U> dataClass);
