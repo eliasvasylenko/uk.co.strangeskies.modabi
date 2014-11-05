0b32c9cee49e0e90236ad2eb6f596d574e466805
eliasvasylenko <eliasvasylenko@gmail.com>
2014-11-05 17:28:10 +0000

    Updated scoping categories for ProcessingContextImpl caches.

--- modabi-core/src/main/java/uk/co/strangeskies/modabi/schema/processing/impl/ProcessingContextImpl.java
+++ modabi-core/src/main/java/uk/co/strangeskies/modabi/schema/processing/impl/ProcessingContextImpl.java
@@ -6,14 +6,47 @@ import uk.co.strangeskies.utilities.collection.ComputingMap;
 
 public class ProcessingContextImpl {
 	public enum CacheScope {
-		MANAGER_GLOBAL, SCHEMA_GLOBAL, PROCESS_GLOBAL, CHILD_NODES
+		MANAGER, PROCESS, STACK
+	}
+
+	private static class MetaKey<K, V> {
+		private final Class<K> key;
+		private final Class<V> value;
+		private final int hash;
+
+		public MetaKey(Class<K> key, Class<V> value) {
+			this.key = key;
+			this.value = value;
+			hash = key.hashCode() ^ value.hashCode();
+		}
+
+		@Override
+		public boolean equals(Object that) {
+			if (!(that instanceof MetaKey))
+				return false;
+
+			MetaKey<?, ?> thatMetaKey = (MetaKey<?, ?>) that;
+
+			return key.equals(thatMetaKey.key) && value.equals(thatMetaKey.value);
+		}
+
+		@Override
+		public int hashCode() {
+			return hash;
+		}
+	}
+
+	private final ComputingMap<MetaKey<?, ?>, ComputingMap<?, ?>> metaMap;
+
+	public ProcessingContextImpl() {
+		metaMap = null;
 	}
 
 	/**
 	 * Returns a unique buffer map for each key/value pair class requested. The
 	 * same underlying data is accessed regardless of the computation function and
 	 * scope requested.
-	 * 
+	 *
 	 * @param key
 	 *          The class of key from which to compute and reference buffered
 	 *          values.
@@ -27,8 +60,9 @@ public class ProcessingContextImpl {
 	 *          map view should be retained within.
 	 * @return
 	 */
+	@SuppressWarnings("unchecked")
 	public final <K, V> ComputingMap<K, V> getBufferMap(Class<K> key,
 			Class<V> value, Function<K, V> computation, CacheScope scope) {
-		return null;
+		return (ComputingMap<K, V>) metaMap.get(new MetaKey<>(key, value));
 	}
 }
