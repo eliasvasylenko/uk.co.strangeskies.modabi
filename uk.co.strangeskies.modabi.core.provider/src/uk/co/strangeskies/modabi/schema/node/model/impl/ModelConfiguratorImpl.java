ca2ebe9dc9da861aaaa96370343fa28be413f93c
Elias N Vasylenko <eliasvasylenko@gmail.com>
2015-03-27 16:50:55 +0000

    License formatting in source files.

--- uk.co.strangeskies.modabi.core.provider/src/uk/co/strangeskies/modabi/schema/node/model/impl/ModelConfiguratorImpl.java
+++ uk.co.strangeskies.modabi.core.provider/src/uk/co/strangeskies/modabi/schema/node/model/impl/ModelConfiguratorImpl.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2015 Elias N Vasylenko <eliasvasylenko@gmail.com>
+ *
+ * This file is part of uk.co.strangeskies.modabi.core.provider.
+ *
+ * uk.co.strangeskies.modabi.core.provider is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.strangeskies.modabi.core.provider is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with uk.co.strangeskies.modabi.core.provider.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package uk.co.strangeskies.modabi.schema.node.model.impl;
 
 import java.util.ArrayList;
