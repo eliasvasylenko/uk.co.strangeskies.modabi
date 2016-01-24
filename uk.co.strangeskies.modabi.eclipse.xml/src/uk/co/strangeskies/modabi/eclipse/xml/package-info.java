314495bf9212db36c0ae6c61f0348a8a0d4efabc
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-01-24 20:29:31 +0000

    Added eclipse xml plugin project with unimplemented skeleton plugin.xml

---
+++ uk.co.strangeskies.modabi.eclipse.xml/src/uk/co/strangeskies/modabi/eclipse/xml/package-info.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 Scientific Analysis Instruments Limited <contact@saiman.co.uk>
+ *
+ * This file is part of uk.co.saiman.msapex.
+ *
+ * uk.co.saiman.msapex is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * uk.co.saiman.msapex is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+@org.osgi.annotation.versioning.Version("1.0.0")
+package uk.co.strangeskies.modabi.eclipse.xml;
