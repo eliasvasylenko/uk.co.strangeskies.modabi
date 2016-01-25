9648a342d9b3cf2c6b6400f885b858488647d0f8
Elias N Vasylenko <eliasvasylenko@gmail.com>
2016-01-25 10:37:28 +0000

    Project configuration now works properly with PlatformAdminImpl etc.

--- uk.co.strangeskies.modabi.eclipse/src/uk/co/strangeskies/modabi/eclipse/PlatformAdminImplComponent.java
+++
@@ -1,9 +0,0 @@
-package uk.co.strangeskies.modabi.eclipse;
-
-import org.eclipse.osgi.compatibility.state.PlatformAdminImpl;
-import org.eclipse.osgi.service.resolver.PlatformAdmin;
-import org.osgi.service.component.annotations.Component;
-
-@SuppressWarnings("restriction")
-@Component(service = PlatformAdmin.class)
-public class PlatformAdminImplComponent extends PlatformAdminImpl {}
